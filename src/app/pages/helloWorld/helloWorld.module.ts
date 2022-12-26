127cc035d30c661c7af9c9f297427048beb377dd
tsuji <xenepic.takku@gmail.com>
2022-12-26 17:02:20 +0900

    一旦実装まで。nodesの情報は表示されているが、ツリー表記はされていない状態

--- src/app/pages/helloWorld/helloWorld.module.ts
+++ src/app/pages/helloWorld/helloWorld.module.ts
@@ -5,13 +5,15 @@ import { IonicModule } from '@ionic/angular';
 
 import { HelloWorldPage } from './helloWorld';
 import { HelloWorldPageRoutingModule } from './helloWorld-routing.module';
+import { TreeModule } from 'angular-tree-component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HelloWorldPageRoutingModule
+    HelloWorldPageRoutingModule,
+    TreeModule.forRoot()
   ],
   declarations: [
     HelloWorldPage,
